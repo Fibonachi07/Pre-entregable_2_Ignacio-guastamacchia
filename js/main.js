9571e149f4d10c688f1c0c259bce854545ac6a36
Ignacio-Guastamacchia <ignacioguastamacchia@gmail.com>
2024-03-01 01:59:52 -0300

    ULTIMAS ACTUALIZACIONES

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     let cards = [];
-    // Load cards from localStorage if available
+    // Carga el localStorage si es que hay.
     const cardsLS = localStorage.getItem("cards");
     if (cardsLS) {
-        try {
             cards = JSON.parse(cardsLS);
-            // Render existing cards on the page
             renderCards(cards);
-        } catch (error) {
-            console.error("Error parsing cards from localStorage:", error);
-        }
     }
 
     function renderCards(cardsArray) {
@@ -20,16 +15,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function addEmployee(event) {
         event.preventDefault();
-        // Generate a unique identifier for the card
+        // Genera un ID único para cada empleado.
         const timestamp = Date.now().toString();
-        // Get form inputs
+        
         const firstNameInput = document.querySelector('input[name="firstName"]');
         const lastNameInput = document.querySelector('input[name="lastName"]');
         const emailInput = document.querySelector('input[name="email"]');
         const phoneInput = document.querySelector('input[name="cellphone"]');
         const positionInput = document.querySelector('input[name="position"]');
         const genderInputs = document.querySelectorAll('input[name="gender"]');
-        // Extract values
+
+        // Valores de los inputs
         const firstName = firstNameInput.value;
         const lastName = lastNameInput.value;
         const email = emailInput.value;
@@ -41,11 +37,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 gender = input.value;
             }
         });
+        const inputs = [firstNameInput, lastNameInput, emailInput, phoneInput, positionInput];
+        inputs.forEach(input => {
+            if (!input.value.trim()) {
+                input.classList.add('error');
+            } else {
+                input.classList.remove('error');
+            }
+        });
+
         if (!firstName || !lastName || !email || !phone || !position || !gender) {
-            alert("Please fill out all fields.");
-            return;
+            return; 
         }
-        // Create a new employee card
+        // Nueva ficha de empleado
         const newCardHTML = `
             <div class="card" data-id="${timestamp}">
                 <div class="card__header">
@@ -72,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function() {
         clearInputs();
     }
 
-    // Function card removal
+    // Remover ficha
     function removeEmployee(event) {
         const cardToRemove = event.target.closest('.card');
         if (cardToRemove) {
@@ -83,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Function card editing
+    // Editar ficha
     function editEmployee(event) {
         const card = event.target.closest('.card');
         if (card) {
@@ -105,10 +109,10 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Function card update
+    // Actualizar ficha
     function updateEmployee(event) {
         event.preventDefault();
-
+    
         const firstName = document.querySelector('input[name="firstName"]').value;
         const lastName = document.querySelector('input[name="lastName"]').value;
         const email = document.querySelector('input[name="email"]').value;
@@ -116,41 +120,29 @@ document.addEventListener("DOMContentLoaded", function() {
         const position = document.querySelector('input[name="position"]').value;
         const gender = document.querySelector('input[name="gender"]:checked').value;
     
-        if (!firstName || !lastName || !email || !phone || !position || !gender) {
-            alert("Please fill out all fields.");
-            return;
-        }
     
-        const updatedCard = document.createElement('div');
-        updatedCard.classList.add('card');
+        // Encontrar la carta a la cuál se está modificando
+        const cardId = document.querySelector('.card--editing').dataset.id;
+        const cardToUpdate = document.querySelector(`.card[data-id="${cardId}"]`);
     
-        updatedCard.innerHTML = `
-            <div class="card__header">
-                <div class="card__info">
-                    <h2 class="card__fullname">${firstName} ${lastName}</h2>
-                    <p class="position">${position}</p>
-                </div>
-            </div>
-            <div class="card__contact">
-                <p class="card__email">Email: ${email}</p>
-                <p class="card__cellphone">Phone Number: ${phone}</p>
-                <p class="card__gender">Gender: ${gender}</p>
-            </div>
-            <div class="displayButtons">
-                <button class="editBtn">🖊️</button>
-                <button class="removeBtn">❌</button>
-            </div>
-        `;
+        if (cardToUpdate) {
+            cardToUpdate.querySelector('.card__fullname').textContent = `${firstName} ${lastName}`;
+            cardToUpdate.querySelector('.position').textContent = position;
+            cardToUpdate.querySelector('.card__email').textContent = `Email: ${email}`;
+            cardToUpdate.querySelector('.card__cellphone').textContent = `Phone Number: ${phone}`;
+            cardToUpdate.querySelector('.card__gender').textContent = `Gender: ${gender}`;
     
-        const cardToUpdate = document.querySelector('.card--editing');
-        const cardContainer = cardToUpdate.parentElement;
-        cardContainer.replaceChild(updatedCard, cardToUpdate);
-    
-        updatedCard.classList.remove('card--editing');
-        clearInputs()
-   
-    }
+            // Actualiza los valores en el localStorage. 
+            const cardIndex = cards.findIndex(card => card.id === cardId);
+            if (cardIndex !== -1) {
+                cards[cardIndex].html = cardToUpdate.outerHTML;
+                localStorage.setItem('cards', JSON.stringify(cards));
+            }
 
+            cardToUpdate.classList.remove('card--editing');
+            clearInputs();
+        }
+    }
     function clearInputs() {
         const inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="tel"], input[type="radio"]');
         inputs.forEach(input => {
@@ -166,17 +158,16 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.addBtn').addEventListener('click', addEmployee);
     document.querySelector('.updateBtn').addEventListener('click', updateEmployee);
 
-    // Remove button
+    // Boton eliminar
     document.addEventListener('click', function(event) {
         if (event.target.classList.contains('removeBtn')) {
             removeEmployee(event);
         }
     });
 
-    // Edit button
+    // Boton Editar
     document.addEventListener('click', function(event) {
         if (event.target.classList.contains('editBtn')) {
-            // Clear previous editing styles
             const editingCard = document.querySelector('.card--editing');
             if (editingCard) {
                 editingCard.classList.remove('card--editing');
